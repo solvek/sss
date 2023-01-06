36695bf23749cfec3bacda5d435b4a7ff5ce5274
solvek <adamchuk@gmail.com>
2023-01-06 12:42:53 +0200

    Registry in local csv file

---
+++ BlackoutCSVRegistry.py
@@ -0,0 +1,29 @@
+import csv
+
+
+class BlackoutCSVRegistry:
+    def __init__(self, file):
+        self.file = file
+        for row in self.rows():
+            self.recent_timestamp = float(row[0])
+            self.is_on = int(row[1])
+
+    def add_record(self, is_on, timestamp):
+        with open(self.file, mode='a') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow([timestamp, is_on])
+
+        self.recent_timestamp = timestamp
+        self.is_on = is_on
+
+    def rows(self):
+        is_header = True
+        with open(self.file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                if is_header:
+                    is_header = False
+                else:
+                    yield row
+                    self.recent_timestamp = float(row[0])
+                    self.is_on = int(row[1])
---
+++ Toolkit.py
@@ -0,0 +1,29 @@
+import os
+from Config import Config
+
+
+class Toolkit:
+    def __init__(self):
+        self.location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        self.scv_registry = None
+        self.gs_registry = None
+        self.config = Config(self.path("solvek.cfg"))
+
+    def path(self, filename):
+        return os.path.join(self.location, filename)
+
+    def csv_registry(self):
+        if self.scv_registry is None:
+            from BlackoutCSVRegistry import BlackoutCSVRegistry
+            self.scv_registry = BlackoutCSVRegistry(self.path("blackouts.csv"))
+        return self.scv_registry
+
+    def gs_registry(self):
+        if self.gs_registry is None:
+            from BlackoutGSRegistry import BlackoutGSRegistry
+            # sheet_name = 'DevSheet' if config.is_test() else 'GatewayPetrushky'
+            config_spreadsheet = self.config.get_section('GOOGLE_SPREADSHEET')
+            sheet_name = config_spreadsheet['SheetName']
+            self.gs_registry = BlackoutGSRegistry(config_spreadsheet, sheet_name)
+
+        return self.gs_registry
